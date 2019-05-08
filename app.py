40b7c7e55ddc146084de790e8f3f061dfbdcec03
Cristiano Singulani <crisingulani@gmail.com>
2019-05-08 13:38:02 -0300

    Initiating code refactoring.

--- app.py
+++ app.py
@@ -1,64 +1,57 @@
-import tornado.web
-import tornado.websocket
-import tornado.ioloop
-import logging
-import time
-from state import State
+from tornado import websocket, web, ioloop
 import json
-from threading import Thread
-import os
+import logging
 
 logger = logging.getLogger('__main__')
 handler = logging.StreamHandler()
 formatter = logging.Formatter(
-    '%(asctime)s %(message)s')
+    '%(asctime)s: %(message)s'
+)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
 
-s = State()
+clients = list()
 
-class WebSocketHandler(tornado.websocket.WebSocketHandler):
-    def open(self):
-        logger.info("New client connected")
-        s.count+=1
-        status = {'status': "connected"}
-        self.write_message(json.dumps(status))
-        self.lock=False
-        self.loop = tornado.ioloop.PeriodicCallback(
-            self.check_ten_seconds, int(os.environ.get('UPDATE_TIME', 1000)))
-        self.loop.start()
 
-    async def on_message(self, message):
-        logger.info(message)
-        if message == "getAllImages":
-            self.write_message(self.get_state())
-        elif message == "clearImages":
-            s.exps = []
-        elif message == "findImages":
-            t = Thread(target=s.monitor_files, args=(s,))
-            t.start()
+class IndexHandler(web.RequestHandler):
+    def get(self):
+        self.render("index.html")
 
-    def on_close(self):
-        logger.info("Client disconnected")
-        s.count-=1
-        self.loop.stop()
 
+class WebSocketHandler(websocket.WebSocketHandler):
     def check_origin(self, origin):
         return True
 
-    def check_ten_seconds(self):
-        # logger.info("Just check")
-        self.write_message(self.get_state())
+    def open(self):
+        if self not in clients:
+            clients.append(self)
 
-    def get_state(self):
-        return json.dumps({"USERS": s.count, "images": s.exps})
-
-application = tornado.web.Application([
-    (r"/", WebSocketHandler),
-], debug=True)
+    def on_close(self):
+        if self in clients:
+            clients.remove(self)
 
 
-if __name__ == "__main__":
-    application.listen(5678)
-    tornado.ioloop.IOLoop.instance().start()
+class ApiHandler(web.RequestHandler):
+
+    async def get(self, *args):
+        self.finish()
+        _id = self.get_argument("id")
+        value = self.get_argument("value")
+        data = {"id" : _id, "value" : value}
+        data = json.dumps(data)
+        for cl in clients:
+            cl.write_message(data)
+
+    async def post(self):
+        pass
+
+app = web.Application([
+    (r'/', IndexHandler),
+    (r'/ws', WebSocketHandler),
+    (r'/api', ApiHandler),
+])
+
+if __name__ == '__main__':
+    app.listen(5678)
+    ioloop.IOLoop.instance().start()
