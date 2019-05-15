468b16e39aeee29fbb332079bb15602b02a74ebe
Cristiano Singulani <crisingulani@gmail.com>
2019-05-15 10:49:02 -0300

    Created image arrival tracking

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 from tornado import websocket, web, ioloop
+from tornado.platform.asyncio import AnyThreadEventLoopPolicy
+from watchdog.observers import Observer
+from watchdog.events import RegexMatchingEventHandler
+import asyncio
 import json
+import os
+import time
 import logging
+from threading import Thread
 
-logger = logging.getLogger('__main__')
-handler = logging.StreamHandler()
+logger = logging.getLogger(__name__)
+handler = logging.FileHandler('expviewer.log')
 formatter = logging.Formatter(
-    '%(asctime)s: %(message)s'
+    '%(asctime)s: %(name)s ~ %(levelname)s: %(message)s'
 )
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
 
 clients = list()
+nuser = int()
 
 
 class IndexHandler(web.RequestHandler):
@@ -20,38 +28,130 @@ class IndexHandler(web.RequestHandler):
 
 
 class WebSocketHandler(websocket.WebSocketHandler):
+    tasks = list()
+
     def check_origin(self, origin):
         return True
 
     def open(self):
         if self not in clients:
+            logger.info('Open request: user {}'.format(self.user_id))
+            global nuser
+            nuser = nuser + 1
+            self.user_id = nuser
             clients.append(self)
 
     def on_close(self):
         if self in clients:
+            logger.info('Close request: user {}'.format(self.user_id))
             clients.remove(self)
+            cancel_tasks(self)
 
 
-class ApiHandler(web.RequestHandler):
+class ImageWatcher:
+    """ Responsible for managing events in images
+    on a particular path.
+    """
+    
+    def __init__(self, src_path):
+        """ Initializes attributes and start thread for the
+        monitoring
+        
+        Arguments:
+            src_path {str} -- directory path to be monitored
+        """
+        self.__src_path = src_path
+        self.__event_handler = ImageHandler()
+        self.__event_observer = Observer()
+        self.__stop_thread = False
+        self.__process = Thread(target=self.__run)
+        self.__process.start()
 
-    async def get(self, *args):
-        self.finish()
-        _id = self.get_argument("id")
-        value = self.get_argument("value")
-        data = {"id" : _id, "value" : value}
-        data = json.dumps(data)
+    def __run(self):
+        """ Sets the background monitoring. """
+        self.__start()
+        try:
+            while True:  
+                if self.__stop_thread: 
+                    break
+                time.sleep(1)
+        except Exception:
+            logger.exception("Image watcher interrupted")
+        finally:
+            self.__stop()
+
+    def __start(self):
+        """ Starts monitoring. """
+        self.__schedule()
+        self.__event_observer.start()
+        logger.info('Image watcher started!')
+
+    def __stop(self):
+        """ Breaks monitoring. """
+        self.__event_observer.stop()
+        self.__event_observer.join()
+        logger.info('Image watcher stoped!')
+
+    def __schedule(self):
+        """ Schedules the event handler. """
+        self.__event_observer.schedule(
+            self.__event_handler,
+            self.__src_path,
+            recursive=True
+        )
+
+    def close(self):
+        """ Sets flag to stop monitoring. """
+        self.__stop_thread = True
+
+
+class ImageHandler(RegexMatchingEventHandler):
+    # FILE_REGEX = [r".*\.fits$"]
+    FILE_REGEX = [r".png$", r".jpg$"]
+
+    def __init__(self):
+        super().__init__(self.FILE_REGEX)
+
+    def on_created(self, event):
+        asyncio.set_event_loop(asyncio.new_event_loop())
+        file_size = -1
+
+        while file_size != os.path.getsize(event.src_path):
+            file_size = os.path.getsize(event.src_path)
+            time.sleep(2)
+
+        filename = os.path.basename(event.src_path)
+        logger.info("{} image created!".format(filename))
+
         for cl in clients:
-            cl.write_message(data)
+            logger.info("-> User {}: send!".format(cl.user_id))
+            task = cl.write_message({'filename': filename})
+            cl.tasks.append(task)
 
-    async def post(self):
-        pass
+        asyncio.get_event_loop().stop()
 
 app = web.Application([
     (r'/', IndexHandler),
     (r'/ws', WebSocketHandler),
-    (r'/api', ApiHandler),
 ])
 
+
+def cancel_all_tasks():
+    global clients
+    for cl in clients:
+        cancel_tasks(cl)
+
+
+def cancel_tasks(cl):
+    for task in cl.tasks:
+        task.cancel()
+
+
 if __name__ == '__main__':
-    app.listen(5678)
-    ioloop.IOLoop.instance().start()
+    try:
+        exp = ImageWatcher('.') 
+        app.listen(5678)
+        ioloop.IOLoop.instance().start()
+    except KeyboardInterrupt:
+        exp.close()
+        cancel_all_tasks()
