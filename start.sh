9ca348b18a3a4363f1547278795392635e1a2c0d
singulani <singulani@srvnode03.linea.gov.br>
2019-07-30 17:21:34 -0300

    Investigating error in the simulation of images. (continue)

--- start.sh
+++ start.sh
@@ -11,39 +11,51 @@ mkdir -p $WATCHERDIR'/tmp';
 
 n=0
 
-find $WATCHERDIR'/tmp' -mindepth 1 -type d -exec rm -rf {} \;
+#function rm_tmp {
+#    find $WATCHERDIR'/tmp' -mindepth 1 -type d -exec rm -rf {} \;
+#}
 
 while true
 do
+    #last_exp=$(printf %03d $n);
 
     n=$(( n+1 ))
+
+    exp=$(printf %03d $n);
 
     echo 'EXP: '$n
     sleep 2;
 
     for i in `ls $IMAGEDIR/*.tif | sort -R`
     do
-        filename=`basename $i`;
-        exp=$(printf %03d $n);
-       	
         mkdir -p $WATCHERDIR'/tmp/'$exp;
-        ln $i $WATCHERDIR'/tmp/'$exp'/exp-'$exp'-'$filename;
-        # cp $i $WATCHERDIR'/'$exp'/exp-'$exp'-'$filename;
+
+        filename=`basename $i`; 
+	file=$WATCHERDIR'/tmp/'$exp'/exp-'$exp'-'$filename;
+
+	if [ -f "$file" ]
+	then
+	    rm -r $file
+	fi
+
+        ln $i $file;
+        # cp $i $file;
     done
 
     echo 'copy pass'
-
+    # to allow 5 seconds of navigation
     sleep 5;
+
+    # remove last exposure
+    #if [ $last_exp != "000" ]
+    #then
+    # 	rm -r $WATCHERDIR'/tmp/'$last_exp;
+    #fi
 
     if [ $n -eq 10 ]
     then
         n=0
         echo 'restarting simulation...'
-        
-        sleep 5;
-        find $WATCHERDIR'/tmp' -mindepth 1 -type d -exec rm -rf {} \;
-        
-        echo 'removed directories'
         echo 'done'
     fi
 
